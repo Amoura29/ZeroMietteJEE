653baaf04c3a6ec657ee1283c45c7d63f0fd0fc1
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-04-04 20:33:10 +0100

    UserService complete

--- src/main/java/mvcModel/UserService.java
+++ src/main/java/mvcModel/UserService.java
@@ -1,9 +1,14 @@
 package mvcModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.User;
 import jakarta.ejb.LocalBean;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 /**
  * Session Bean implementation class UserService
@@ -19,5 +24,40 @@ public class UserService {
     public UserService() {
         // TODO Auto-generated constructor stub
     }
-
+    public User getUserbyId(int id) {
+    	User user=em.find(User.class, id);
+    	return user;
+    }
+    public List<User> getAllUserbyState(String state){
+    	List<User> users=new ArrayList<User>();
+    	TypedQuery<User> query =em.createNamedQuery("User.findAllbyState",User.class);
+    	query.setParameter(1, state);
+    	users=query.getResultList();
+    	return users;
+    }
+    public String deleteUserById(int id){
+    	User user = em.find(User.class, id);
+    	if(user!=null)
+    		em.remove(user);
+    	else return "introuvable";
+    	user = em.find(User.class, id);
+    	if(user!=null)
+    		return "failure";
+    	return "success";
+    }
+    public void addUser(String add,String des,String ema,String fn,String ln,String num,byte[] pro,String pwd,String reg,String r,String state) {
+    	User u=new User();
+    	u.setAddress(add);
+    	u.setDescription(des);
+    	u.setEmail(ema);
+    	u.setFirstName(fn);
+    	u.setLastName(ln);
+    	u.setNumber(num);
+    	u.setProof(pro);
+    	u.setPwd(pwd);
+    	u.setRegion(reg);
+    	u.setRole(r);
+    	u.setState(state);
+    	em.persist(u);
+    }
 }
